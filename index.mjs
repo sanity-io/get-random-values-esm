e4918c824b6372317909cd889d5126c5c32bf6fd
Espen Hovlandsdal <espen@hovlandsdal.com>
2024-02-08 14:44:21 -0800

    fix: try global `crypto` if no `window` is defined in ESM module

--- index.mjs
+++ index.mjs
@@ -1,5 +1,14 @@
 // Strict ESM env, designed to run outside Node.js in envs that provide WebCrypto (deno, browsers, etc)
 
 export default function getRandomValues(typedArray) {
-  return window.crypto.getRandomValues(typedArray)
+  const crypto =
+    typeof window !== 'undefined' && 'crypto' in window
+      ? window.crypto
+      : globalThis.crypto
+
+  if (!crypto || !crypto.getRandomValues) {
+    throw new Error('WebCrypto not available in this environment')
+  }
+
+  return getRandomValues(typedArray)
 }
